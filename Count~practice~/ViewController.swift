b003ecf1bd34b2ba72ce2d1eb7a7f13dc396f1a1
Yasunori Noguchi <ngssive1215@gmail.com>
2016-11-16 19:57:30 +0900

    first commit

--- Count~practice~/ViewController.swift
+++ Count~practice~/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,87 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func Plus(){
+        if number + 1 >= 999999 {
+            number = 999999
+        }
+        else {
+            number = number + 1
+        }
+        label.text = String(number)
+        if number > 0 {
+            label.textColor = UIColor.red
+        }
+        else if number > 0 {
+            label.textColor = UIColor.blue
+        }
+        else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func Minus(){
+        if number - 1 >= 999999 {
+            number = 999999
+        }
+        else {
+            number = number - 1
+        }
+        label.text = String(number)
+        if number > 0 {
+            label.textColor = UIColor.red
+        }
+        else if number < 0 {
+            label.textColor = UIColor.blue
+        }
+        else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func Square(){
+        if number * number >= 999999 {
+            number = 999999
+        }
+        else {
+            number = number * number
+        }
+        label.text = String(number)
+        if number > 0 {
+            label.textColor = UIColor.red
+        }
+        else if number < 0 {
+            label.textColor = UIColor.blue
+        }
+        else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func Devide(){
+        if number / 2 >= 999999 {
+            number = 999999
+        }
+        else {
+            number = number / 2
+        }
+        label.text = String(number)
+        if number > 0 {
+            label.textColor = UIColor.red
+        }
+        else if number < 0 {
+            label.textColor = UIColor.blue
+        }
+        else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func AC() {
+        number = 0
+        label.text = String(number)
+        label.textColor = UIColor.black
+    }
 }
 
